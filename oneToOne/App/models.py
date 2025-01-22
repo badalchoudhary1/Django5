3a1fdab7e8a4d8290d69fbd1cdeb095dffe50f0f
badalchoudhary1 <badalror10@gmail.com>
2025-01-22 14:23:36 +0530

    add OneToOne concept

---
+++ oneToOne/App/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
